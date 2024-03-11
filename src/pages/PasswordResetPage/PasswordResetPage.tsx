9ced222b796f829a123d6786868e66368ef06a2c
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-03-10 23:53:42 -0400

    ts bug fix

--- src/pages/PasswordResetPage/PasswordResetPage.tsx
+++ src/pages/PasswordResetPage/PasswordResetPage.tsx
@@ -1,15 +1,15 @@
 import Navbar from "../../componets/UI/Navbar/Navbar";
 import Footer from "../../componets/UI/Footer/Footer";
 import "./PasswordResetPage.css";
-import { useState } from "react";
+// import { useState } from "react";
 const PasswordResetPage = () => {
-  const [heading, setHeading] = useState("Enter your email address");
+  // const [heading, setHeading] = useState("Enter your email address");
   return (
     <>
       <Navbar />
       <div className="password-reset-page-container">
         <div className="password-reset-container">
-          <h2>{heading}</h2>
+          <h2>Enter Your Email</h2>
           <div className="email-input">
             <input type="text" placeholder="Email" />
           </div>
