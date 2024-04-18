9e6166c998e36fec258016703fef5a5c360a1601
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2024-04-18 17:11:35 -0400

    fixed some ts errors 4

--- src/componets/BookedEvents/BookedEvents.tsx
+++ src/componets/BookedEvents/BookedEvents.tsx
@@ -7,6 +7,8 @@ import { IVenue } from "../../IVenue";
 import useApi from "../../hooks/apiHook";
 import { useDispatch } from "react-redux";
 import { IBookedEvent, setBookedEvent } from "../../store/booked-event-store";
+import LoadingModal from "../UI/Modal/LoadingModal";
+import { loadingActions } from "../../store/loading-store";
 
 const BookedEvents = () => {
   const [venues, setVenues] = useState<IVenue[]>([]);
@@ -14,6 +16,7 @@ const BookedEvents = () => {
   const { getAllVenues, retrieveAllReservations } = useApi();
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(loadingActions.setLoading({ isLoading: true, message: "" }));
     getAllVenues().then(async (res) => {
       const response = await res;
       setVenues(response);
@@ -22,6 +25,7 @@ const BookedEvents = () => {
   }, []);
   useEffect(() => {
     retrieveAllReservations().then((res) => {
+      dispatch(loadingActions.setLoading({ isLoading: false, message: "" }));
       setBookedEvents(res);
       let data: IBookedEvent[] = [];
 
@@ -34,8 +38,8 @@ const BookedEvents = () => {
         data.push({
           eventId: event.id,
           eventName: event.name,
-          eventDate: event.startTime.split("T")[0],
-          eventTime: event.startTime.split("T")[1],
+          eventDate: event.startTime?.toString().split("T")[0],
+          eventTime: event.startTime?.toString().split("T")[1],
           coverImg: event.coverImg,
           eventLocation: `${identifiedVenue?.street}, ${identifiedVenue?.city}, ${identifiedVenue?.state}, ${identifiedVenue?.zipcode}`,
           venueName: identifiedVenue?.name,
@@ -43,41 +47,47 @@ const BookedEvents = () => {
       });
       dispatch(setBookedEvent(data));
     });
-  }, []);
+  }, [venues.length > 0]);
   console.log(bookedEvents);
 
   return (
-    <div className="booked-events-container">
-      <h2>Your Booked Events</h2>
-      <div className="booked-events">
-        {bookedEvents.map((event, index) => (
-          <div key={index} className="booked-event">
-            <div className="event-image">
-              <img src={event.coverImg} alt={event.name} />
+    <>
+      <div className="booked-events-container">
+        <h2>Your Booked Events</h2>
+        <div className="booked-events">
+          {bookedEvents.map((event, index) => (
+            <div key={index} className="booked-event">
+              <div className="event-image">
+                <img src={event.coverImg} alt={event.name} />
+              </div>
+              <div className="event-key-details">
+                <h3>{event.name}</h3>
+                <p>
+                  {
+                    venues.find((venue: any) => venue.id === event.venueId)
+                      ?.name
+                  }
+                </p>
+                <p>{event.startTime.split("T")[0]}</p>
+              </div>
+              <div className="manage-event">
+                <button>
+                  <Link
+                    to={{
+                      pathname: "/manage-event",
+                    }}
+                    state={{ bookedEventId: event.id, userId: event.userId }}
+                  >
+                    Manage Event
+                  </Link>
+                </button>
+              </div>
             </div>
-            <div className="event-key-details">
-              <h3>{event.name}</h3>
-              <p>
-                {venues.find((venue: any) => venue.id === event.venueId)?.name}
-              </p>
-              <p>{event.startTime.split("T")[0]}</p>
-            </div>
-            <div className="manage-event">
-              <button>
-                <Link
-                  to={{
-                    pathname: "/manage-event",
-                  }}
-                  state={{ bookedEventId: event.id, userId: event.userId }}
-                >
-                  Manage Event
-                </Link>
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <LoadingModal message="Loading your booked events." />
+    </>
   );
 };
 
